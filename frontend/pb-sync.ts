8cdd7157406a923a9ac1d64d8d3cda8129f19179
Vladislav Mihov <vladislav.mihov@syna.pt>
2024-02-03 03:22:15 +0200

    feat(frontend): create pages for each pb collection

--- frontend/pb-sync.ts
+++ frontend/pb-sync.ts
@@ -6,6 +6,143 @@ import Handlebars from 'handlebars';
 
 dotenv.config();
 
+// Helper to capitalize field names
+Handlebars.registerHelper('capitalize', (str) => str && str.charAt(0).toUpperCase() + str.slice(1));
+
+// Helper for conditional logic
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+  switch (operator) {
+    case '===':
+      return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    // Add other operators as needed
+    default:
+      return options.inverse(this);
+  }
+});
+
+const templateSource = fs.readFileSync(path.join(__dirname, 'templates', 'table.hbs'), 'utf8');
+const template = Handlebars.compile(templateSource);
+
+const data = {
+  name: 'Opala',
+  schema: [
+        {
+            "system": false,
+            "id": "nzjerais",
+            "name": "text_field",
+            "type": "text",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "min": null,
+                "max": null,
+                "pattern": ""
+            }
+        },
+        {
+            "system": false,
+            "id": "oad2vbfm",
+            "name": "number_field",
+            "type": "number",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "min": null,
+                "max": null,
+                "noDecimal": false
+            }
+        },
+        {
+            "system": false,
+            "id": "rkdhsynh",
+            "name": "boolean_field",
+            "type": "bool",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {}
+        },
+        {
+            "system": false,
+            "id": "h51hvpjt",
+            "name": "email_field",
+            "type": "email",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "exceptDomains": null,
+                "onlyDomains": null
+            }
+        },
+        {
+            "system": false,
+            "id": "9nfo473d",
+            "name": "url_field",
+            "type": "url",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "exceptDomains": null,
+                "onlyDomains": null
+            }
+        },
+        {
+            "system": false,
+            "id": "nlzakewt",
+            "name": "datetime_field",
+            "type": "date",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "min": "",
+                "max": ""
+            }
+        },
+        {
+            "system": false,
+            "id": "roeludrd",
+            "name": "select_field",
+            "type": "select",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "maxSelect": 1,
+                "values": [
+                    "choice1",
+                    "choice2",
+                    "choice3"
+                ]
+            }
+        },
+        {
+            "system": false,
+            "id": "ol7ufgxj",
+            "name": "relation_field",
+            "type": "relation",
+            "required": false,
+            "presentable": false,
+            "unique": false,
+            "options": {
+                "collectionId": "_pb_users_auth_",
+                "cascadeDelete": false,
+                "minSelect": null,
+                "maxSelect": 1,
+                "displayFields": null
+            }
+        }
+    ]
+}
+
+const tableComponent = template(data);
+console.log(tableComponent);
+fs.writeFileSync(path.join(__dirname, 'app', 'table.tsx'), tableComponent);
+
 const postAuth = async () => {
     const authEndpoint = 'http://localhost:8090/api/admins/auth-with-password';
     const authData = {
@@ -52,7 +189,6 @@ const getCollections = async (token: string) => {
         }
 
         const collections = await response.json();
-        console.log(collections);
         generateFiles(collections);
 
     } catch (error) {
@@ -60,7 +196,7 @@ const getCollections = async (token: string) => {
     }
 };
 
-function generateFiles(data: object) {
+function generateFiles(data) {
     // Register a helper function for the 'startsWith' conditional
     Handlebars.registerHelper('startsWith', (value, prefix) => value.startsWith(prefix));
 
@@ -70,7 +206,22 @@ function generateFiles(data: object) {
 
     // Generate the navbar component
     const navbarComponent = template(data);
+    const collections = data.items.filter(collection => !collection.id.startsWith('_pb_'));
 
+    collections.forEach(collection => {
+        const dirPath = path.join(__dirname, 'app', collection.name);
+
+        // Check if the directory already exists to avoid overwriting it
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+            console.log(`Directory created: ${dirPath}`);
+        }
+        const pageTemplateSrc = fs.readFileSync(path.join(__dirname, 'templates', 'page.hbs'), 'utf8');
+        const pageTemplate = Handlebars.compile(pageTemplateSrc);
+        const pageComponent = pageTemplate(collection);
+        fs.writeFileSync(path.join(dirPath, 'page.tsx'), pageComponent);
+
+    })
     // Write to a file
     fs.writeFileSync(path.join(__dirname, 'app', 'menubar.tsx'), navbarComponent);
 }
